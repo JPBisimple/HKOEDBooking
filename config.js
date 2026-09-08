f3b331d49edaca9ba13242bcedb74c532b171aae
JPBisimple <janp@bisimple.dk>
2026-09-08 21:37:56 +0200

    Update config.js
    Opdatering af URL

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
 // ligger i Row Level Security, ikke i at skjule nøglen.
 // Brug ALDRIG en secret key (sb_secret_...) her.
 window.APP_CONFIG = {
-  SUPABASE_URL: "sb_publishable_fKSeNOLoDttXqclRooHijQ_yV7YXA8E",
+  SUPABASE_URL: "https://brxooqnvondphgpiwaet.supabase.co",
   SUPABASE_PUBLISHABLE_KEY: "sb_publishable_DIN_NOEGLE_HER"
 };
